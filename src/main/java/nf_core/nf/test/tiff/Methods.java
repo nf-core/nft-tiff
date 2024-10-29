726b50c04415b42fff4f307ed2223817f6d6227b
Tom Sellman <tom.sellman@seqera.io>
2024-10-29 16:46:56 +0100

    Add tiff library
    https://ngageoint.github.io/tiff-java/

--- src/main/java/nf_core/nf/test/tiff/Methods.java
+++ src/main/java/nf_core/nf/test/tiff/Methods.java
@@ -1,15 +1,17 @@
 package nf_core.nf.test.tiff;
 
+import mil.nga.tiff.TIFFImage;
+
 /*
  * Add your custom methods to nf-test
  * 
  * @author: nf-core
  */
-
 public class Methods {
 
     public static void hello() {
         System.out.println("Hello from nft-tiff");
+        TIFFImage img = new TIFFImage();
     }
 
 }
